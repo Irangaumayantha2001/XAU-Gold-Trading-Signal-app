b2b455229847be0af16f4204d4182cd243f97917
WellageIranga2001 <studentiranga@gmail.com>
2026-08-30 22:00:34 +0530

    add mobile app

--- backend/database.py
+++ backend/database.py
@@ -3,11 +3,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 
-load_dotenv()  # This reads the .env file
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+# This creates the connection to Supabase
+engine = create_engine(DATABASE_URL, echo=True)  # echo=True shows SQL in terminal
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
